cb025f1838917260f47787f042a37969698e6701
Jim R <6371524+jimtheguy@users.noreply.github.com>
2026-03-30 18:18:32 -0500

    Add files via upload

---
+++ SCT.ino
@@ -0,0 +1,185 @@
+/* LUX Sensing Module for measuring steel strip cleanliness */
+
+
+
+/* TSL2591 Digital Light Sensor */
+/* Dynamic Range: 600M:1 */
+/* Maximum Lux: 88K */
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+
+/* SSD1306 1.3" 128x64 monochrome OLED Display*/
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the lux sensor ID
+
+/* Output sensor details to serial*/
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Configures the gain and integration time for the TSL2591
+*/
+/**************************************************************************/
+void configureSensor(void)
+{
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  //tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+  
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+
+  /* Display the gain and integration time for reference sake */  
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Gain:         "));
+  tsl2591Gain_t gain = tsl.getGain();
+  switch(gain)
+  {
+    case TSL2591_GAIN_LOW:
+      Serial.println(F("1x (Low)"));
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println(F("25x (Medium)"));
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println(F("428x (High)"));
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println(F("9876x (Max)"));
+      break;
+  }
+  Serial.print  (F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+  Serial.println(F(" ms"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+}
+
+/**************************************************************************/
+/*
+    Read from the sensor
+*/
+/**************************************************************************/
+/* Define the array for storing sensor readings*/
+const int numReadings = 5;
+int sensorReading[numReadings];
+
+void setup(void) 
+{
+  Serial.begin(9600);
+
+  //wait for display
+  delay(500);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.begin();
+  display.display();
+  //delay(500); // Pause for 0.5 seconds
+  display.clearDisplay();
+
+   // Clear the buffer
+  display.clearDisplay();
+  delay(500); // Pause for 0.5 seconds
+  Serial.println(F("Starting Adafruit TSL2591"));
+  display.print(F("Starting Sensor"));
+  display.display();
+  
+  if (tsl.begin()) 
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+  } 
+  else 
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1);
+  }
+    
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Configure the sensor */
+  configureSensor();
+
+  /* Read from sensor and store in array sensorReading[] */
+  uint32_t lux, lum;
+  uint16_t ir, full;
+
+  for(int i = 0; i < numReadings; i++){
+    lum = tsl.getFullLuminosity();
+    ir = lum >> 16;
+    full = lum & 0xFFFF;
+    sensorReading[i] = tsl.calculateLux(full, ir);
+    Serial.print(F("Reading "));
+    Serial.print(i + 1);
+    Serial.print(F(": "));
+    Serial.println(sensorReading[i]);
+    display.clearDisplay();
+    display.setTextSize(1);             // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE);        // Draw white text
+    display.setCursor(0,0);   
+    display.print(F("Reading "));
+    display.print(i + 1);
+    display.print(F(": "));
+    display.println(sensorReading[i]);
+    display.display();
+    delay(500);
+  }
+  long sum = 0;
+  for(int i = 0; i < numReadings; i++){
+    sum += sensorReading[i];
+  }
+  float average = (float)sum / numReadings;
+  Serial.print(F("Average value: ")); Serial.print(average);
+  display.clearDisplay();
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);   
+  display.println(F("Average Lux: ")); 
+  display.println(F(" "));
+  display.setTextSize(2); display.print(average);
+  display.display();
+}
+
+void loop() {
+  // loop is not utilized
+
+}
